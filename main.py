85cf690a29d5b5c0a4f89a251c639c7c835cedc7
yadavaavik <yadavaavik10@gmail.com>
2026-03-29 00:19:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import telebot
 import os
+from pymongo import MongoClient
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+MONGO_URI = os.getenv("MONGO_URI")
 
 bot = telebot.TeleBot(BOT_TOKEN)
+
+client = MongoClient(MONGO_URI)
+db = client["earnbot"]
+
+users = db["users"]
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "🚀 Welcome to Earn Bot!")
+    user_id = message.from_user.id
+
+    user = users.find_one({"user_id": user_id})
+
+    if not user:
+        users.insert_one({
+            "user_id": user_id,
+            "balance": 0,
+            "wallet": None,
+            "created_at": message.date
+        })
+
+    bot.reply_to(message, "✅ You are registered!")
 
 
 print("Bot running...")
