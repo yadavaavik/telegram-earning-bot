e1caef029e252f727ae51a891aaf350410e2d0b4
yadavaavik <yadavaavik10@gmail.com>
2026-03-29 19:53:23 +0530

    Update force_join.py

--- utils/force_join.py
+++ utils/force_join.py
@@ -1,14 +1,34 @@
-CHANNELS = ["@milkyxbubble"]
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
-async def check_join(update, context):
-    user_id = update.effective_user.id
+CHANNELS = [
+    "@milkyxbubble"  # <-- put your channel
+]
 
-    for ch in CHANNELS:
+ADMIN_ID = 8250329715 # <-- your telegram id
+
+
+async def check_force_join(bot, user_id):
+    if user_id == ADMIN_ID:
+        return True
+
+    for channel in CHANNELS:
         try:
-            member = await context.bot.get_chat_member(ch, user_id)
+            member = await bot.get_chat_member(channel, user_id)
             if member.status in ["left", "kicked"]:
                 return False
         except:
             return False
 
     return True
+
+
+def get_join_buttons():
+    buttons = []
+
+    for channel in CHANNELS:
+        link = f"https://t.me/{channel.replace('@','')}"
+        buttons.append([InlineKeyboardButton("📢 Join Channel", url=link)])
+
+    buttons.append([InlineKeyboardButton("✅ Check", callback_data="check_join")])
+
+    return InlineKeyboardMarkup(buttons)
