1d860deabb621d9ac1588f51ff40b5b3a01599d1
yadavaavik <yadavaavik10@gmail.com>
2026-03-29 18:06:01 +0530

    Create mongo.py

---
+++ database/mongo.py
@@ -0,0 +1,8 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_URI
+
+client = AsyncIOMotorClient(MONGO_URI)
+db = client["earning_bot"]
+
+users = db["users"]
+withdraws = db["withdraws"]
