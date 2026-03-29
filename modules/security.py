6bb11f0d8ab2cc07ce905969e46d3d1152bfe00e
yadavaavik <yadavaavik10@gmail.com>
2026-03-29 19:01:19 +0530

    Update security.py

--- modules/security.py
+++ modules/security.py
@@ -5,13 +5,11 @@ COOLDOWN = 300  # 5 minutes
 
 async def can_withdraw(user_id):
     user = await users.find_one({"user_id": user_id})
-
     now = int(time.time())
     last = user.get("last_withdraw", 0)
 
     if now - last < COOLDOWN:
         return False
-
     return True
 
 async def set_withdraw_time(user_id):
