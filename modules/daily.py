773de192495000063e7c7f132aed715a40896b11
yadavaavik <yadavaavik10@gmail.com>
2026-03-29 18:35:24 +0530

    Update daily.py

--- modules/daily.py
+++ modules/daily.py
@@ -2,18 +2,14 @@ import time
 from database.mongo import users
 from modules.balance import add_balance
 
-DAILY_BONUS = 1
-
 async def claim_daily(user_id):
     user = await users.find_one({"user_id": user_id})
-
     now = int(time.time())
-    last = user.get("last_daily", 0)
 
-    if now - last < 86400:
+    if now - user.get("last_daily", 0) < 86400:
         return False
 
-    await add_balance(user_id, DAILY_BONUS)
+    await add_balance(user_id, 1)
 
     await users.update_one(
         {"user_id": user_id},
