a3c0a91aa68c8f7e30157bd386c68b0c255ab436
yadavaavik <yadavaavik10@gmail.com>
2026-03-29 18:37:57 +0530

    Update handlers.py

--- core/handlers.py
+++ core/handlers.py
@@ -1,9 +1,7 @@
 from telegram.ext import CommandHandler, CallbackQueryHandler, MessageHandler, filters
-
 from handlers.start import start_cmd
 from handlers.button import button_handler
 from handlers.message import msg_handler
-from handlers.tasks import show_tasks, task_click
 
 def register_handlers(app):
     app.add_handler(CommandHandler("start", start_cmd))
