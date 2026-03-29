0724d00a68247d821468c79966feec243a3d8f35
yadavaavik <yadavaavik10@gmail.com>
2026-03-29 18:36:22 +0530

    Update crypto.py

--- payments/crypto.py
+++ payments/crypto.py
@@ -1,7 +1,4 @@
 import random
 
-# 🔥 Replace later with real API
 async def send_crypto(wallet, amount):
-    # TODO: integrate CryptoBot API here
-    # For now safe simulation
     return f"TX{random.randint(1000000,9999999)}"
