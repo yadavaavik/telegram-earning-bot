b5d0276eb3cf6c133302ca33319e3f5db8734887
yadavaavik <yadavaavik10@gmail.com>
2026-03-29 17:44:06 +0530

    Delete keyboards directory

--- keyboards/menu.py
+++
@@ -1,7 +0,0 @@
-from telegram import InlineKeyboardMarkup, InlineKeyboardButton
-
-def main_menu():
-    return InlineKeyboardMarkup([
-        [InlineKeyboardButton("👤 Profile", callback_data="profile")],
-        [InlineKeyboardButton("💸 Withdraw", callback_data="withdraw")]
-    ])
