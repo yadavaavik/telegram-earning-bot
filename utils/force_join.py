6fb0b74b6c7a9666a062d765954e4a823d925dd2
yadavaavik <yadavaavik10@gmail.com>
2026-03-29 18:27:03 +0530

    Create force_join.py

---
+++ utils/force_join.py
@@ -0,0 +1,17 @@
+CHANNELS = [
+    "@yourchannel1",
+    "@yourchannel2"
+]
+
+async def check_join(update, context):
+    user_id = update.effective_user.id
+
+    for ch in CHANNELS:
+        try:
+            member = await context.bot.get_chat_member(ch, user_id)
+            if member.status in ["left", "kicked"]:
+                return False
+        except:
+            return False
+
+    return True
