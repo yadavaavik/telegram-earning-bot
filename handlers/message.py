6041c4cbff3032e98f2fef6ace2b04b14a2f91a8
yadavaavik <yadavaavik10@gmail.com>
2026-03-29 17:43:57 +0530

    Delete handlers directory

--- handlers/message.py
+++
@@ -1,7 +0,0 @@
-from telegram.ext import MessageHandler, filters
-
-async def msg_handler(update, context):
-    await update.message.reply_text("📩 Message received")
-
-def register(app):
-    app.add_handler(MessageHandler(filters.TEXT, msg_handler))
